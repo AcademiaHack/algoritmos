56e310dab16ccb9a1555605e36e6c79fa0527f14
Romer Ramos <rramos@4geeks.co>
2014-11-03 16:45:33 -0430

    Fix en el archivo de jose para mejorar la identacion y fixes en declaracion de variables

--- jsalazar/test2.js
+++ jsalazar/test2.js
@@ -1,48 +1,49 @@
-var p = " (s para SI y n para NO)";
+var input;
+var p;
 
-var input = prompt("Piensa en un país de américa del sur y escribe la letra s minuscula cuando estés listo");
+p = "(s para SI y n para NO)";
 
-var input = prompt("hablan portugés?" +p);
+//input = prompt("Piensa en un país de américa del sur y escribe la letra s minuscula cuando estés listo");
+input = prompt("hablan portugés?" +p);
+
 if (input=='s') {
 	console.log("Tu país es Brasil");
-}
-else if (input=='n') {
-	var input = prompt("Tienen mar?" +p);
-	if (input == 'n') {console.log("Tu país es Bolivia");}
-	else if (input=='s') {
-		var input = prompt("Tienen muchas miss universo?" +p);
-		if (input=='s') {console.log("Tu país es Venezuela");}
-		else if (input=='n') {
-		var input = prompt("Producen café?" +p);
-		if (input=='s') {
-			console.log("Tu país es Colombia");
-		}
-		else if (input=='n') {
-			var input = prompt("Allí está Machu Pichu?" +p);
-			if (input == 's') {
-				console.log("Tu país es Perú");
-			}
-			else if (input=='n') {
-				var input = prompt("Tu presidente es Correa?" +p);
+} else if (input=='n') {
+	input = prompt("Tienen mar?" +p);
+	if (input == 'n') {
+		console.log("Tu país es Bolivia");
+	} else if (input=='s') {
+			input = prompt("Tienen muchas miss universo?" +p);
+			if (input=='s') {
+				console.log("Tu país es Venezuela");
+			} else if (input=='n') {
+			input = prompt("Producen café?" +p);
+			if (input=='s') {
+				console.log("Tu país es Colombia");
+			} else if (input=='n') {
+				input = prompt("Allí está Machu Pichu?" +p);
 				if (input == 's') {
-					console.log("Tu país es Ecuador");
-				}
-				else if (input=='n') {
-					var input = prompt("Eres un pibe?" +p);
+					console.log("Tu país es Perú");
+				} else if (input=='n') {
+					input = prompt("Tu presidente es Correa?" +p);
 					if (input == 's') {
-						console.log("Tu país es Argentina");
-					}
-					else if (input=='n') {
-						var input = prompt("Te burlas de los argentinos?"+p);
+						console.log("Tu país es Ecuador");
+					} else if (input=='n') {
+						input = prompt("Eres un pibe?" +p);
 						if (input == 's') {
-							console.log("Tu país es Uruguay")
+							console.log("Tu país es Argentina");
+						} else if (input=='n') {
+							input = prompt("Te burlas de los argentinos?"+p);
+							if (input == 's') {
+								console.log("Tu país es Uruguay")
+							} else {
+								console.log("Tu país es Chile");
+							}
 						}
-						else {console.log("Tu país es Chile");}
 					}
 				}
 			}
 		}
-	}
 	}
 	
 }
